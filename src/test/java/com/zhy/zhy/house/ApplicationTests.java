94d2586e25e2fa78b6200e0c97b584d52c913119
@zhangyu07036283 <2838635235@qq.com>
2020-12-24 22:19:23 +0800

    first

---
+++ src/test/java/com/zhy/zhy/house/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhy.zhy.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
